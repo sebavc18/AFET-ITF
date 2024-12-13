a0f966396cb497ec4f4f3ca5d192e236bc460afd
Sebastian Villalba <villalbasebastianc@gmail.com>
2024-12-13 15:27:35 -0300

    animacion a la seccion nosotros

--- js/main.js
+++ js/main.js
@@ -24,3 +24,4 @@ ingreso.addEventListener("click", function (){
 
 
 
+
--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,34 @@
-// Seleccionar el botón y el menú
 const hamburger = document.querySelector('.hamburger');
 const barnav = document.querySelector('.barnav');
 
-// Evento de clic para alternar el menú
+// Evento para alternar el menú
 hamburger.addEventListener('click', () => {
     barnav.classList.toggle('active');
+});
+
+let isImageVisible = false; // Flag para la imagen
+let isTextVisible = false;  // Flag para el texto
+
+window.addEventListener('scroll', function () {
+    const elements = [
+        { id: '#myImage', flag: 'isImageVisible' },
+        { id: '#texto', flag: 'isTextVisible' }
+    ];
+
+    const windowHeight = window.innerHeight;
+
+    elements.forEach(el => {
+        const element = document.querySelector(el.id);
+        const position = element.getBoundingClientRect().top;
+
+        if (position < windowHeight && !window[el.flag]) {
+            window[el.flag] = true;
+            element.classList.remove('hidden');
+            element.classList.add('visible');
+        } else if (position >= windowHeight && window[el.flag]) {
+            window[el.flag] = false;
+            element.classList.remove('visible');
+            element.classList.add('hidden');
+        }
+    });
 });
